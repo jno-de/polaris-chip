05a72a22bcc59bf54aac44b402cca3a3b412835b
jno-de <joshua.n.owen@outlook.com>
2024-09-23 15:38:47 -0400

    added some cards

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,10 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
+    this.img = "";
     this.title = "My card";
+    this.desc = "My description";
+    this.link = ""
   }
 
   static get styles() {
@@ -22,16 +25,101 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+      .control-wrapper {
+        padding: 10px;
+      }
+
+      .card {
+        background-color: #E0ECD4;
+        width: 300px;
+        padding: 20px;
+        margin: auto;
+        text-align: center;
+        vertical-align: middle;
+        border: 2px solid grey;
+      }
+
+      .fancy {
+        background-color: pink;
+        color: dimgray;
+        border: 10px solid purple;
+      }
+
+      img {
+        border: 3px solid #3B6F03;
+        width: 150px;
+      }
+
+      .text {
+        font-family: Arial;
+        background-color: White;
+        border: 2px solid Black;
+      }
+
+      button {
+        font-family: Arial;
+        color: White;
+        background-color: #1BA0E1;
+        border-width: 2px;
+        border-radius: 4px;
+        border-color: #0F6C97;
+        padding: 6px;
+      }
+
+      h2 {
+        font-family: Arial;
+      }
+
+      #card-list {
+        display: flex;
+      }
+
+      @media screen and (min-width: 500px) and (max-width: 800px) {
+        .card-button {
+          display: none;
+        }
+      }
+
+      @media screen and (max-width: 500px) {
+        .card {
+          width: 90%; 
+          padding: 10px;
+        }
+
+        img {
+          width: 100px;
+          height: 100px;
+        }
+
+        h1 {
+          font-size: 24px;
+        }
+
+        p {
+          font-size: 16px;
+        }
+      }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div class="card">
+      <img src=${this.img}>
+      <h1 class="text">${this.title}</h1>
+      <p class="text">${this.desc}</p>
+      <a href=${this.link}>
+          <button class="card-button"><b>Details</b></button>
+      </a>
+    </div>`;
   }
 
   static get properties() {
     return {
+      img: { type : String },
       title: { type: String },
+      desc: { type: String },
+      link: { type : String },
     };
   }
 }
